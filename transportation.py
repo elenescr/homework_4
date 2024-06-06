861206e77b0f1b0718f1e92daba01d8cdc4e8b07
Elene Zurabiani <elene.zurabiani906@ens.tsu.ge>
2024-06-06 05:25:47 +0400

    1 and 2

--- transportation.py
+++ transportation.py
@@ -1,1 +1,11 @@
+class Transport:
+    steering_wheel = 1
+    weight = 5000
+    windshield = 1
+    headlights = 2
+    @staticmethod
+    def door_lock():
+        print("The doors are locked")
+
+
 
